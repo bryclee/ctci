2dd4dd88a5a59522b1c49581eabfe13b0aa44830
Bryan Lee <pygmize.me@gmail.com>
2015-08-17 00:29:03 -0700

    [solution] Adds to 8-1 solution

--- spec/8_OOP/8-1_cardsSpec.js
+++ spec/8_OOP/8-1_cardsSpec.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 describe('deck of cards', function() {
 	var Cards = require('../../src/8_OOP/8-1_cards.js');
 
-	it('should have proper classes', function() {
+	it('should have Deck, Card, and Hand classes', function() {
 		expect(Cards.Deck).to.be.a('function');
 		expect(Cards.Card).to.be.a('function');
 		expect(Cards.Hand).to.be.a('function');
@@ -12,27 +12,83 @@ describe('deck of cards', function() {
 	describe('deck', function() {
 		var deck = new Cards.Deck();
 
-		it('should have methods to handle cards', function() {
+		it('should be able to initialize with 52 cards', function() {
+			expect(deck.initialize).to.be.a('function');
+
+			deck.initialize();
+
+			expect(deck.cards.length).to.equal(52);
+		});
+
+		it('should be able to shuffle the cards', function() {
 			expect(deck.shuffle).to.be.a('function');
+
+			var originalOrder = deck.cards.slice();
+
+			deck.shuffle();
+
+			expect(deck.cards).to.not.eql(originalOrder);
+		});
+
+		it('should be able to deal cards', function() {
 			expect(deck.dealCard).to.be.a('function');
+
+			var dealtCard = deck.dealCard();
+
+			expect(dealtCard).to.be.an.instanceOf(Cards.Card);
+		});
+
+		it('should be able to deal a hand with cards', function() {
 			expect(deck.dealHand).to.be.a('function');
+
+			var dealtHand = deck.dealHand(3); //Hand.prototype.addCard should be stubbed out
+
+			expect(dealtHand).to.be.an.instanceOf(Cards.Hand);
+			expect(dealtHand.cards.length).to.equal(3);
 		});
 	});
 
 	describe('hand', function() {
-		var hand = new Cards.Hand();
+		var hand;
+		beforeEach(function() {
+			hand = new Cards.Hand();
+		});
 
 		it('should have an array of cards', function() {
 			expect(hand.cards).to.be.an.instanceOf(Array);
 		});
+
+		it('should be able to add cards', function() {
+			expect(hand.addCard).to.be.a('function');
+
+			hand.addCard(new Cards.Card(0, 0));
+
+			expect(hand.cards.length).to.equal(1);
+		});
 	});
 
 	describe('card', function() {
-		var card = new Cards.Card(0, 0);
+		var card = new Cards.Card(1, 0);
 
 		it('should have a value and suit', function() {
 			expect(card.value).to.be.a('number');
 			expect(card.suit).to.be.a('number');
+		});
+
+		it('should be able to get the name of the card', function() {
+			expect(card.getName).to.be.a('function');
+
+			var name = card.getName();
+
+			expect(name).to.equal('Ace');
+		});
+
+		it('should be able to get the suit of the card', function() {
+			expect(card.getSuit).to.be.a('function');
+
+			var suit = card.getSuit();
+
+			expect(suit).to.equal('Diamonds');
 		});
 	});
 
--- src/8_OOP/8-1_cards.js
+++ src/8_OOP/8-1_cards.js
@@ -1,23 +1,65 @@
 module.exports = (function() {
+	var NAMES = {
+		1: 'Ace',
+		11: 'Jack',
+		12: 'Queen',
+		13: 'King'
+	};
+
+	var SUITS = {
+		0: 'Diamonds',
+		1: 'Clubs',
+		2: 'Hearts',
+		3: 'Spades'
+	};
+
 	var Deck = function() {
 		this.cards = []; //array that holds all the cards currently in the deck
 		/* can instantiate deck with 52 cards for playing card deck */
 	};
 
+	Deck.prototype.initialize = function() {
+		for (var suit in SUITS) {
+			for (var i = 1; i <= 13; i++) {
+				this.cards.push(new Card(i, suit));
+			}
+		}
+	};
+
 	Deck.prototype.shuffle = function() {
-		/* ... */
+		var randomIndex;
+		var temp;
+		for (var i = 0; i < this.cards.length; i++) {
+			randomIndex = Math.floor(Math.random() * (this.cards.length - i)) + i;
+			temp = this.cards[i];
+			this.cards[i] = this.cards[randomIndex];
+			this.cards[randomIndex] = temp;
+		}
 	};
 
 	Deck.prototype.dealCard = function() {
-		/* ... */
+		return this.cards.pop();
 	};
 
-	Deck.prototype.dealHand = function() {
-		/* ... */
+	Deck.prototype.dealHand = function(n) {
+		n = n || 0;
+
+		var hand = new Hand();
+
+		for ( ; n > 0; n--) {
+			hand.addCard(this.dealCard());
+		}
+
+		return hand;
 	};
 
 
 	var Card = function(value, suit) {
+		if (value === undefined || suit === undefined) {
+			//Throw error for required values
+			return null;
+		}
+
 		this.value = value;
 		this.suit = suit;
 		this.revealed = true;
@@ -27,9 +69,21 @@ module.exports = (function() {
 		this.revealed = !this.revealed;
 	};
 
+	Card.prototype.getName = function() {
+		return NAMES[this.value];
+	};
 
-	var Hand = function() {
-		this.cards = [];
+	Card.prototype.getSuit = function() {
+		return SUITS[this.suit];
+	};
+
+
+	var Hand = function(cards) {
+		this.cards = cards || [];
+	};
+
+	Hand.prototype.addCard = function(card) {
+		this.cards.push(card);
 	};
 
 
